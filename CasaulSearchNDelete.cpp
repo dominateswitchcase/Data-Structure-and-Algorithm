eca52e013129b8a7e0c18c9b0559c4dab06fb1a0
Dominic Casaul <rowendominiccasaul@gmail.com>
2025-10-18 22:15:37 +0800

    Add linked list implementation with search and delete
    Introduces a LinkedList class supporting node insertion, deletion, search, and search-and-delete operations. Includes a main function for interactive command-based manipulation of the list.

---
+++ CasaulSearchNDelete.cpp
@@ -0,0 +1,133 @@
+//CASAUL SEARCH AND DELETE
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+    Node(int val) : data(val), next(nullptr) {}
+};
+
+class LinkedList {
+public:
+    int inputSize;
+    Node* head;
+    LinkedList() : head(nullptr), inputSize(0) {}
+
+    ~LinkedList() {
+        refreshList();
+    }
+
+    void insertNode(int val) {
+        Node* newNode = new Node(val);
+        newNode->next = head;
+        head = newNode;
+        inputSize++;
+    }
+
+    bool deleteNode() {
+        if (head == nullptr) {
+            cout << "THE LIST IS ALREADY EMPTY" << endl;
+            return false;
+        }
+        Node* temp = head;
+        head = head->next;
+        delete temp;
+        inputSize--;
+        return 0;
+    }
+
+    void refreshList() {
+        while (head != nullptr) {
+            Node* temp = head;
+            head = head->next;
+            delete temp;
+        }
+        inputSize = 0;
+    }
+    void searchList(int val){
+      Node* current = head; 
+        while (current != nullptr) { 
+            if (current->data == val) {
+            cout << "VALUE FOUND" << endl;
+                 return; 
+            }
+            current = current->next; 
+        }
+        cout<<"VALUE NOT FOUND"<< endl;
+}
+
+
+    void searchNdelete(int val){
+         Node* input = head;
+         Node* current = head->next; 
+        if(this->head->data == val){
+              Node* temp = head;
+                 head = current->next;
+                delete temp;
+                inputSize--;
+                printList();
+                return;
+        }
+        while (current != nullptr) { 
+            if (current->data == val) {
+                input->next = current->next;
+                delete current;
+                inputSize--;
+                printList();
+                return;
+            }
+            input= current;
+            current = current->next; 
+           
+                cout<<"VALUE NOT FOUND"<< endl;
+            
+        }
+    }
+
+    void printList() const {
+        cout << "[" << inputSize << "]";
+        if (inputSize > 0) cout << " ";
+        Node* current = head;
+        while (current != nullptr) {
+            cout << current->data;
+            if (current->next != nullptr) cout << " ";
+            current = current->next;
+        }
+        cout << endl;
+    }
+};
+
+int main() {
+    LinkedList* list = new LinkedList(); 
+    char command;
+    int num;
+
+    while (cin >> command >> num) {
+        if (command == 'i') {
+            list->insertNode(num);  
+            list->printList();      
+        } else if (command == 'd') {
+            if (list->head == nullptr) {  
+                cout << "THE LIST IS ALREADY EMPTY" << endl;
+            } else {
+                list->deleteNode();  
+                list->printList();   
+            }
+        } else if (command == 'r') {
+            list->refreshList();   
+            list->printList();    
+        } else if(command == 's'){
+           list->searchList(num);
+        } else if(command== 'x'){
+            list->searchNdelete(num);
+        }
+        else {
+            cout << "INVALID COMMAND" << endl;
+        }
+    }
+
+    delete list;  
+    return 0;
+}
